979437c220bfb360e5faef9ff02a1394bbb0554b
Florian <florian.kamps@epitech.eu>
2021-01-05 11:48:54 +0100

    fix fetch test

--- test/fetch.test.js
+++ test/fetch.test.js
@@ -10,6 +10,13 @@ const time = low(adapter)
 describe("Integration test", function() {
     it("should be able to fetch dynamically all projects", function() {
         getTimeline();
-        assert.notStrictEqual(moment(time.get('fetchTime').value()), moment());
+        setTimeout(() => {
+            time.read()
+            let time1 = moment(time.get('fetchTime').value()).toDate();
+            time1 = time1.toString();
+            let actual = moment().subtract(1, 'seconds').toDate();
+            actual = actual.toString();
+            assert.strictEqual(time1, actual);
+        }, 1000);
     });
 });
